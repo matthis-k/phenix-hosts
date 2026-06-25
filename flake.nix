ab1f59a1749833a8b7216284267e31f93cc77fb8
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 13:46:15 +0200

    Simplify flake to minimal outline

--- flake.nix
+++ flake.nix
@@ -3,9 +3,6 @@
 
   inputs = {
     phenix-pins.url = "github:matthis-k/phenix-pins";
-    phenix-packages.url = "github:matthis-k/phenix-packages";
-    phenix-shell.url = "github:matthis-k/phenix-shell";
-    phenix-nvim.url = "github:matthis-k/phenix-nvim";
     nixpkgs.follows = "phenix-pins/nixpkgs";
   };
 
