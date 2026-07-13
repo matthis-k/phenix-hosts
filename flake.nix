7f871a3f48c65cadf2ad2edef9090d267e7fd184
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 01:43:08 +0200

    refactor: separate flake output concerns

--- flake.nix
+++ flake.nix
@@ -2,10 +2,16 @@
   description = "Phenix NixOS host configurations";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     phenix-pins.url = "github:matthis-k/phenix-pins";
     nixpkgs.follows = "phenix-pins/nixpkgs";
-    phenix-tend.url = "github:matthis-k/phenix-tend";
+    phenix-tend = {
+      url = "github:matthis-k/phenix-tend";
+      inputs.phenix-pins.follows = "phenix-pins";
+    };
     home-manager.follows = "phenix-pins/home-manager";
     sops-nix.follows = "phenix-pins/sops-nix";
     disko = {
@@ -21,7 +27,11 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
-      imports = [ ./modules/package.nix ];
+      imports = [
+        ./modules/outputs.nix
+        ./modules/package.nix
+        ./modules/development.nix
+      ];
       flake.flakeModules.default = import ./modules/flake-module.nix;
     };
 }
