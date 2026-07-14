4cf13f816afe50d47f7e6799dc07d189d1d4fae8
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 03:53:44 +0200

    fix: use native nixpkgs NordVPN integration

--- flake.nix
+++ flake.nix
@@ -47,11 +47,6 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nordvpn-flake = {
-      url = "github:connerohnesorge/nordvpn-flake/f802a2efd8225116158371a8c85db28e7b0846dd";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
