71877dede99e80626cada638264b8ecd83e6a277
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 04:58:20 +0200

    refactor: compose hosts and user through Den

---
+++ modules/den.nix
@@ -0,0 +1,40 @@
+{
+  den,
+  inputs,
+  lib,
+  ...
+}:
+let
+  workstationModule = import ./nixos/workstation.nix { inherit inputs; };
+  laptopModule = import ./nixos/hosts/laptop.nix;
+  desktopModule = import ./nixos/hosts/desktop.nix;
+  matthiskHostModule = import ./nixos/users-matthisk.nix { inherit inputs; };
+  matthiskHomeModule = import ./home/matthisk.nix { inherit inputs; };
+in
+{
+  den.schema.user.classes = lib.mkDefault [ "homeManager" ];
+
+  den.hosts.x86_64-linux = {
+    matthisk-laptop-newxos.users.matthisk = { };
+    matthisk-desktop-newxos.users.matthisk = { };
+  };
+
+  den.aspects = {
+    workstation.nixos = workstationModule;
+
+    matthisk-laptop-newxos = {
+      includes = [ den.aspects.workstation ];
+      nixos = laptopModule;
+    };
+
+    matthisk-desktop-newxos = {
+      includes = [ den.aspects.workstation ];
+      nixos = desktopModule;
+    };
+
+    matthisk = {
+      homeManager = matthiskHomeModule;
+      provides.to-hosts.nixos = matthiskHostModule;
+    };
+  };
+}
