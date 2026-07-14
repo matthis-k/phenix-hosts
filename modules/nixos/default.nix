9dba424b33969579a050a5fede3514c7ce6394cf
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 03:03:32 +0200

    feat: make workstation the default host profile

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,12 +1,4 @@
 { inputs }:
 {
-  imports = [
-    (import ./home-manager.nix { inherit inputs; })
-    (import ./sops.nix { inherit inputs; })
-    ./nix-base.nix
-    ./users-matthisk.nix
-    ./locale-de-en.nix
-    ./audio-pipewire.nix
-    ./sudo-wheel-passwordless.nix
-  ];
+  imports = [ (import ./workstation.nix { inherit inputs; }) ];
 }
