8a71360f91c0e1944bab343bd22d647510519f2b
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 03:01:11 +0200

    feat: migrate Git and GitHub configuration

---
+++ modules/home/git.nix
@@ -0,0 +1,45 @@
+{ lib, pkgs, ... }:
+{
+  programs.git = {
+    enable = true;
+    package = pkgs.gitFull;
+    settings = {
+      user = {
+        name = "matthis-k";
+        email = "matthis.kaelble@gmail.com";
+      };
+      pull.rebase = false;
+      merge.conflictstyle = "diff3";
+      init.defaultBranch = "main";
+      core.editor = "nvim";
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
+
+  programs.gh.enable = true;
+  programs.lazygit.enable = true;
+
+  programs.fish.interactiveShellInit = lib.mkAfter ''
+    if test -r /run/secrets/github_token
+      set -l github_token (string trim < /run/secrets/github_token)
+
+      if test -n "$github_token"
+        set -gx GH_TOKEN $github_token
+        set -gx GITHUB_TOKEN $github_token
+        set -gx GITHUB_PERSONAL_ACCESS_TOKEN $github_token
+      end
+    end
+  '';
+
+  programs.ssh.settings."github.com" = {
+    AddKeysToAgent = "yes";
+    HostName = "github.com";
+    IdentitiesOnly = true;
+    IdentityFile = "/run/secrets/github_id";
+    User = "git";
+  };
+}
