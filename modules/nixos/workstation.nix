6d4c500e79d6560d8cfcd547288426816c6dd5d3
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 04:59:09 +0200

    refactor: centralize shared workstation policy

--- modules/nixos/workstation.nix
+++ modules/nixos/workstation.nix
@@ -7,7 +7,6 @@
     (import ./home-manager.nix { inherit inputs; })
     (import ./sops.nix { inherit inputs; })
     (import ./nix-base.nix { inherit inputs; })
-    (import ./users-matthisk.nix { inherit inputs; })
     ./localsend.nix
     ./services/nordvpn.nix
     ./locale-de-en.nix
@@ -18,6 +17,18 @@
   ];
 
   phenix.sops.enable = true;
+
+  newxos.nordvpn.enable = true;
+
+  sops.secrets.nordvpn_token = {
+    format = "binary";
+    mode = "0400";
+    path = "/run/secrets/nordvpn_token";
+    sopsFile = ../../secrets/nordvpn_token;
+  };
+
+  services.displayManager.autoLogin.user = "matthisk";
+  system.stateVersion = "25.11";
 
   boot.zfs.forceImportRoot = false;
 
