aab551d6116687d141a710065bf55e0350b38512
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 03:02:34 +0200

    fix: materialize desktop boot configuration

--- modules/nixos/boot/desktop.nix
+++ modules/nixos/boot/desktop.nix
@@ -1,1 +1,25 @@
-bootConfig
+{ pkgs, ... }:
+{
+  boot.initrd = {
+    systemd.enable = true;
+    verbose = false;
+  };
+
+  boot.plymouth = {
+    enable = true;
+    font = "${pkgs.nerd-fonts.hack}/share/fonts/truetype/NerdFonts/Hack/HackNerdFontMono-Regular.ttf";
+  };
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = true;
+      default = "saved";
+    };
+  };
+
+  boot.resumeDevice = "/dev/system/swap";
+}
