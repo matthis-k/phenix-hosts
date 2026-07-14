0ddd76b6b33eaec90f01623b5f89f866b1b7d92f
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 04:59:32 +0200

    refactor: keep desktop module host-specific

--- modules/nixos/hosts/desktop.nix
+++ modules/nixos/hosts/desktop.nix
@@ -1,7 +1,5 @@
-{ inputs }:
 {
   imports = [
-    (import ../workstation.nix { inherit inputs; })
     ../hardware/desktop.nix
     ../storage/desktop.nix
     ../boot/desktop.nix
@@ -12,19 +10,7 @@
 
   phenix.de.hyprland.monitors = [ ];
 
-  newxos.nordvpn = {
-    enable = true;
-    technology = "NORDLYNX";
-  };
-
-  sops.secrets.nordvpn_token = {
-    format = "binary";
-    mode = "0400";
-    path = "/run/secrets/nordvpn_token";
-    sopsFile = ../../../secrets/nordvpn_token;
-  };
+  newxos.nordvpn.technology = "NORDLYNX";
 
   services.llm-server.enableTTS = true;
-  services.displayManager.autoLogin.user = "matthisk";
-  system.stateVersion = "25.11";
 }
