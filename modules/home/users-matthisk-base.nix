7d17b5fa83dac94ba2242d7458742c8b78d279be
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 22:56:10 +0200

    refactor: keep account policy in Home base module

--- modules/home/users-matthisk-base.nix
+++ modules/home/users-matthisk-base.nix
@@ -1,3 +1,19 @@
+{ config, lib, ... }:
 {
+  home = {
+    username = config.phenix.user.name;
+    homeDirectory = config.phenix.user.homeDirectory;
+    stateVersion = config.phenix.versions.homeManager;
+
+    sessionVariables = {
+      PHENIX_ROOT = config.phenix.paths.root;
+      PHENIX_FLAKE = config.phenix.paths.flake;
+      PHENIX_DE_ROOT = config.phenix.paths.desktop;
+    }
+    // lib.optionalAttrs config.phenix.devMode {
+      PHENIX_DEV = "1";
+    };
+  };
+
   programs.home-manager.enable = true;
 }
