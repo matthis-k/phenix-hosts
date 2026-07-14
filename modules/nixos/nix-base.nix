4ea43e17dcfc37bc4e124cb8af3410c3e275973e
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-14 03:00:25 +0200

    feat: migrate workstation Nix policy

--- modules/nixos/nix-base.nix
+++ modules/nixos/nix-base.nix
@@ -1,24 +1,52 @@
+{ inputs }:
 {
+  config,
+  lib,
+  ...
+}:
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = _: true;
+    cudaSupport = true;
+  };
+
   nix = {
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      trusted-users = [
-        "root"
-        "@wheel"
-      ];
-    };
+    registry = lib.mapAttrs (_: flake: { inherit flake; }) (
+      lib.filterAttrs (_: value: lib.isType "flake" value) inputs
+    );
+
+    nixPath = [ "/etc/nix/path" ];
 
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 30d";
+      options = "--delete-older-than 14d";
     };
 
-    optimise.automatic = true;
+    optimise = {
+      automatic = true;
+      dates = [ "weekly" ];
+    };
+
+    settings = {
+      auto-optimise-store = true;
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+    };
   };
 
-  nixpkgs.config.allowUnfree = true;
+  environment.etc = lib.mapAttrs' (name: value: {
+    name = "nix/path/${name}";
+    value.source = value.flake;
+  }) config.nix.registry;
+
+  programs.nix-ld.enable = true;
 }
